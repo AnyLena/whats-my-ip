60d2f2aeb9c40a632767baa8d47d88461c7b1acc
LenaBultmann <lena.bultmann@gmx.de>
2024-01-12 12:38:01 +0100

    adding map and displaying flag

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
-import { useState } from 'react'
-import './App.css'
-import DisplayIp from './components/DisplayIp'
+import { useState, useEffect } from "react";
+import "./App.css";
+import DisplayIp from "./components/DisplayIp";
+import LeafletMap from "./components/LeafletMap";
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userIp, setUserIp] = useState();
+  const [userCountry,setUserCountry]= useState();
 
-  // console.log(import.meta.env.VITE_REACT_KEY_IPIFY)
+  const fetchIp = async () => {
+    try {
+      const response = await axios.get(
+        ` https://geo.ipify.org/api/v2/country,city?apiKey=${
+          import.meta.env.VITE_REACT_KEY_IPIFY
+        }`
+      );
+      setUserIp(response.data);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  const fetchCountry = async () => {
+    try {
+      const response = await axios.get(
+        ` https://restcountries.com/v3.1/alpha/${userIp.location.country}
+        `
+      );
+      setUserCountry(response.data[0]);
+      console.log(response);
+    } catch (error) {
+      console.error("Error fetching data: ", error.message);
+    }
+  };
+
+  console.log(userIp);
+  console.log(userCountry);
+  useEffect(() => {
+    fetchIp();
+  }, []);
+  
+  useEffect(() => {
+    fetchCountry();
+  }, [userIp]);
+ 
   return (
     <>
-      <DisplayIp />
+      {userIp && userCountry && <DisplayIp userIp={userIp} userCountry={userCountry}/>}
+      {userIp && <LeafletMap userIp={userIp}/>}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
